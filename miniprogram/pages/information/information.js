061b13839dc8dc90fb29bbc24d09f91f39c33805
yh <1741349792@qq.com>
2022-04-24 14:21:34 +0800

    2022-4-24下午，完成详情信息页面

--- miniprogram/pages/information/information.js
+++ miniprogram/pages/information/information.js
@@ -1,25 +1,27 @@
-
 import {
   getUserProfile
 } from "../../utils/utils" 
+let phone = wx.getStorageSync("phone")
+let gender = wx.getStorageSync("gender")
+let department = wx.getStorageSync("department")
+let classroom = wx.getStorageSync("classroom")
+let signature = wx.getStorageSync("signature")
+let ownid = wx.getStorageSync('ownid')
 Page({
     /**
      * 页面的初始数据
      */
     data: {
-
-        show:false,    
-        columns: ['男','女'],   
-        gender:'',             //选择男女
-
-        show2:false,
+        show:false,              //性别选择器（关闭）
+        columns: ['男','女'],    //性别选择器
+        gender:'',            
+        show2:false,           //院系选择器（关闭）
         columns2:['双能学院','软件学院','其他'], 
-        department:'',         //选择院系
-
+        department:'',         //院系选择器
         signature:'',          //个性签名
         phone:'',             //11位手机号
-        classroom:'',             //班级，如20机器人02班
-        ownid:''                 //数据库中有用户记录，欲修改信息时获取id
+        classroom:'',         //班级，如20机器人02班
+        ownid:'' //数据库中有用户记录，欲修改信息时获取id
     },
     /**
      * 生命周期函数--监听页面加载
@@ -27,37 +29,42 @@ Page({
     onLoad: function (options) {
     },
     findpeople(){
-       wx.cloud.callFunction({
-         name:'login',
-       })
+      wx.cloud.callFunction({
+          name:'login',
+      })
        .then(res => {
-         wx.cloud.database().collection("information")
-         .where({
-           openid:res.result.openid
-         })
-         .get()
-         .then(res =>{
-           let phone = wx.getStorageSync('phone')
-           let gender = wx.getStorageSync("gender")
-           let department = wx.getStorageSync("department")
-           let classroom = wx.getStorageSync("classroom")
-           let ownid = wx.getStorageSync('ownid')
-           if(res.data.length == 0){
-             console.log("infomation中没有用户记录")
-           }
-           else if(phone.length == 0 || gender.length == 0 ||
+       wx.cloud.database().collection("information")
+       .where({
+         openid:res.result.openid
+       })
+       .get()
+       .then(res =>{
+    if(res.data.length == 0){
+         console.log("infomation中没有用户记录")
+        }
+    else if(phone.length == 0 || gender.length == 0 ||
             department.length == 0 || classroom.length == 0 || ownid.length == 0){
-            console.log("information中有用户记录,但是用户删除了缓存,或Storage中的数据不完整")
-            let ownid = res.data[0]._id
-            let phone = res.data[0].phone
-            let gender = res.data[0].gender
-            let department = res.data[0].department
-            let classroom = res.data[0].classroom
-            let signature = res.data[0].signature
-            this.setData({
-              ownid:ownid,phone:phone,gender:gender,department:department,classroom:classroom,signature:signature
+        console.log("information中有用户记录,但是用户删除了缓存,或Storage中的数据不完整")
+        let ownid = res.data[0]._id
+        let phone = res.data[0].phone
+        let gender = res.data[0].gender
+        let department = res.data[0].department
+        let classroom = res.data[0].classroom
+        let signature = res.data[0].signature
+        this.setData({
+            ownid:ownid,
+            phone:phone,
+            gender:gender,
+            department:department,
+            classroom:classroom,
+            signature:signature
             }) 
-            this.storage()
+        wx.setStorageSync('phone', this.data.phone)
+        wx.setStorageSync('gender', this.data.gender)
+        wx.setStorageSync('department', this.data.department)
+        wx.setStorageSync('classroom', this.data.classroom)
+        wx.setStorageSync('signature', this.data.signature)
+        wx.setStorageSync('ownid', this.data.ownid)
            }
          })
        })
@@ -71,16 +78,23 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow: function () { 
-      let phone = wx.getStorageSync("phone")
-      let gender = wx.getStorageSync("gender")
-      let department = wx.getStorageSync("department")
-      let classroom = wx.getStorageSync("classroom")
-      let signature = wx.getStorageSync("signature")
-      let ownid = wx.getStorageSync('ownid')
+    let phone = wx.getStorageSync("phone")
+    let gender = wx.getStorageSync("gender")
+    let department = wx.getStorageSync("department")
+    let classroom = wx.getStorageSync("classroom")
+    let signature = wx.getStorageSync("signature")
+    let ownid = wx.getStorageSync('ownid')
       if(phone.length != 0 && gender.length != 0 &&
         department.length != 0 && classroom.length != 0 && ownid.length != 0){
       console.log("有用户信息且信息完整")
-      this.setData({phone:phone,gender:gender,department:department,classroom:classroom,signature:signature,ownid:ownid})
+      this.setData({
+          phone:phone,
+          gender:gender,
+          department:department,
+          classroom:classroom,
+          signature:signature,
+          ownid:ownid
+        })
       }
       else{
         this.findpeople()
@@ -112,12 +126,16 @@ Page({
      */
     onShareAppMessage: function () {
     },
-    showPopup(e){      //点击选择性别，打开弹出层（选择器）
+    showPopup(e){      //性别选择器
       this.hideKeyBorder()
-      this.setData({show:true})
+      this.setData({
+          show:true
+        })
       },
-   onClose() {       //点击空白处开闭弹出层（选择器）及选择器左上角的取消
-      this.setData({ show: false });
+   onClose() {   //点击空白处开闭弹出层（选择器）及选择器左上角的取消
+      this.setData({ 
+          show: false 
+        });
     },
    onConfirm(e){     //选择器右上角的确定，点击确定获取值
      this.setData({
@@ -127,7 +145,9 @@ Page({
    },
    showPopupDepartment(e){
      this.hideKeyBorder()
-     this.setData({show2:true})
+     this.setData({
+         show2:true
+        })
    },
     onConfirm2(e){
       this.setData({
@@ -136,7 +156,9 @@ Page({
       })
     },
     onClose2(){
-      this.setData({show2:false})
+      this.setData({
+          show2:false
+        })
     },
     getsignature(e){
       this.data.signature = e.detail
@@ -167,7 +189,7 @@ Page({
         },
       })
     },
-    submit(){
+    submit(){   
       getUserProfile().then(res => {
         if(this.data.phone.length != 11){
           this.setData({
@@ -185,14 +207,19 @@ Page({
            })
          }
          else if(this.data.ownid.length != 0){
-          //用户曾经填写过信息，想要修改信息
-          this.storage()  //将当前编辑完成的数据缓存
+          console.log("用户曾经填写过信息，想要修改信息")
+          wx.setStorageSync('phone', this.data.phone)
+          wx.setStorageSync('gender', this.data.gender)
+          wx.setStorageSync('department', this.data.department)
+          wx.setStorageSync('classroom', this.data.classroom)
+          wx.setStorageSync('signature', this.data.signature)
+          wx.setStorageSync('ownid', this.data.ownid)
           wx.cloud.database().collection("information")
           .doc(this.data.ownid)
           .update({
             data:{
               userAvatarUrl: res.avatarUrl,  //用户头像
-              userNickName: res.nickName,    //用户微信名
+              userNickName: res.nickName,  //用户微信名
               phone: this.data.phone,        //手机号
               signature:this.data.signature,  //个性签名
               gender:this.data.gender,       //性别
@@ -217,13 +244,18 @@ Page({
             console.log(err)
           })
         }  
-        else{
-          this.storage()  //将当前编辑完成的数据缓存
-          wx.cloud.database().collection("information")
+  else{
+    wx.setStorageSync('phone', this.data.phone)
+    wx.setStorageSync('gender', this.data.gender)
+    wx.setStorageSync('department', this.data.department)
+    wx.setStorageSync('classroom', this.data.classroom)
+    wx.setStorageSync('signature', this.data.signature)
+    wx.setStorageSync('ownid', this.data.ownid)
+    wx.cloud.database().collection("information")
           .add({
             data:{
               userAvatarUrl: res.avatarUrl,  //用户头像
-              userNickName: res.nickName,    //用户微信名
+              userNickName: res.nickName,  //用户微信名
               phone: this.data.phone,        //手机号
               signature:this.data.signature,  //个性签名
               gender:this.data.gender,       //性别
@@ -249,13 +281,5 @@ Page({
           })
         }
       })
-    },
-    storage(){          //数据缓存
-      wx.setStorageSync('phone', this.data.phone)
-      wx.setStorageSync('gender', this.data.gender)
-      wx.setStorageSync('department', this.data.department)
-      wx.setStorageSync('classroom', this.data.classroom)
-      wx.setStorageSync('signature', this.data.signature)
-      wx.setStorageSync('ownid', this.data.ownid)
     },
 })
--- miniprogram/pages/mine/mine.js
+++ miniprogram/pages/mine/mine.js
@@ -9,7 +9,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function() {
+  onLoad:function(){ 
     this.getTabBar().setData({
       active : 2
     })
@@ -25,7 +25,9 @@ Page({
   onShow: function(){
     let wxname = wx.getStorageSync('nickName')
     let AvatarUrl = wx.getStorageSync('avatarUrl')
-    this.setData({wxname:wxname})
+    this.setData({
+        wxname:wxname
+    })
     if(this.data.wxname.length == 0){
       this.setData({
         wxname:"尚未完善个人信息",
@@ -69,24 +71,24 @@ Information() {       //个人信息界面
     url: '../information/information',
   })
   },
+  ownupload(){        //我的发布界面
+    wx.navigateTo({
+      url: '../ownupload/ownupload',   
+    })
+  },
+  sold(){             //我的交易界面
+    wx.navigateTo({
+      url: '../sold/sold',   
+    })
+  },
 concerning() {       //关于北跳界面
   wx.navigateTo({
     url: '../concerning/concerning',
   })
 },
-ownupload(){
+guestbook(){        //留言板界面
   wx.navigateTo({
-    url: '../ownupload/ownupload',   //切换页面的路径
-  })
-},
-sold(){
-  wx.navigateTo({
-    url: '../sold/sold',   //切换页面的路径
-  })
-},
-guestbook(){
-  wx.navigateTo({
-    url: '../guestbook/guestbook',   //切换页面的路径
+    url: '../guestbook/guestbook',  
   })
 },
 })
